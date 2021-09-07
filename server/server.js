0c87d0d5f91feefbbf8a8e2b7c2153f3a1026815
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-07 10:42:06 -0500

    backend files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8000;
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+  console.log(`1: Server running on port: ${PORT}`)
+})
