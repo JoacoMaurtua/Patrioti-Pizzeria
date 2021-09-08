c64ea065ef211f28fb9cf89c319601825f2e1366
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-07 23:10:50 -0500

    established connection with db

---
+++ server/config/mongodb.config.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dataBase = 'mongodb://localhost/pizzeriaDB';
+
+const connectDB = async() =>{
+  try{
+    await mongoose.connect(dataBase, {
+      useNewUrlParser: true,
+      useUnifiedTopoly:true
+    });
+
+    console.log(`2: Established connection with data base`);
+
+  }catch(err){
+    console.error(err);
+    process.exit(1) // error opcional
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors'); //SUPER IMPORTANTE
 
 const app = express();
 
 const PORT = 8000;
 
 
+//midlewares:
+
+//para poder usar 2 servers en paralelo
+app.use(cors({credentials:true, origin: 'http://localhost:3000'}))
 
 
 
